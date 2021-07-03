c43a4b726410e79d3d83bcbebed2af40520193ec
broodjeaap89 <davidzessen@gmail.com>
2021-07-03 15:50:29 +0200

    In between commit with numberpickers for number of pizzas and doughball size

--- lib/pages/AddPlannedPizzaPage.dart
+++ lib/pages/AddPlannedPizzaPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class AddPlannedPizzaPage extends StatefulWidget {
   @override
@@ -9,6 +10,8 @@ class AddPlannedPizzaPage extends StatefulWidget {
 class AddPlannedPizzaPageState extends State<AddPlannedPizzaPage> {
   String name = "";
   String pizzaType = "Neapolitan";
+  int pizzaCount = 1;
+  int doughballSize = 200;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,9 +37,7 @@ class AddPlannedPizzaPageState extends State<AddPlannedPizzaPage> {
               value: pizzaType,
               isExpanded: true,
               onChanged: (String? newType) {
-                setState(() {
-                  pizzaType = newType!;
-                });
+                setState(() => pizzaType = newType!);
               },
               items: <String>["Neapolitan", "New York", "Chicago"]
                 .map<DropdownMenuItem<String>>((String v) {
@@ -46,7 +47,41 @@ class AddPlannedPizzaPageState extends State<AddPlannedPizzaPage> {
                 );
               }).toList()
             ),
-
+            Column(
+              children: <Widget>[
+                Text("# Of Pizzas: "),
+                NumberPicker(
+                  value: pizzaCount,
+                  minValue: 1,
+                  maxValue: 1000,
+                  itemHeight: 30,
+                  axis: Axis.horizontal,
+                  onChanged: (newPizzaCount) => setState(() => this.pizzaCount = newPizzaCount),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(color: Colors.black26),
+                  ),
+                )
+              ]
+            ),
+            Column(
+                children: <Widget>[
+                  Text("Doughball Size: "),
+                  NumberPicker(
+                    value: doughballSize,
+                    minValue: 100,
+                    maxValue: 10000,
+                    step: 10,
+                    itemHeight: 30,
+                    axis: Axis.horizontal,
+                    onChanged: (newDoughballSize) => setState(() => this.doughballSize = newDoughballSize),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(color: Colors.black26),
+                    ),
+                  )
+                ]
+            ),
           ]
         )
       )

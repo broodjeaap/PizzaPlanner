1339bbdff2922fb43d24b20f8eb28fa7a5cbae31
broodjeaap89 <davidzessen@gmail.com>
2021-07-09 17:38:26 +0200

    added pizza type to the PizzaEventWidget

--- lib/widgets/PizzaEventWidget.dart
+++ lib/widgets/PizzaEventWidget.dart
@@ -66,7 +66,14 @@ class PizzaEventWidget extends StatelessWidget {
                         ]
                     ),
                   ),
-                  Text(dateFormatter.format(pizzaEvent.dateTime)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(dateFormatter.format(pizzaEvent.dateTime)),
+                      Text(pizzaEvent.type)
+                    ],
+                  ),
+
                 ]
             )
         )
